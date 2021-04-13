5006e71e9076659663b18f1b968cd407e08f43ac
ganesh-48 <ganeshsrananavare@gmail.com>
2021-04-12 22:39:18 -0700

    ADDED: Linked List

---
+++ src/main/java/com/linkedlist/MyNode.java
@@ -0,0 +1,11 @@
+package com.linkedlist;
+
+public class MyNode<k> {
+    private  k key;
+    private MyNode next;
+
+    public MyNode(k key) {
+        this.key = null;
+        this.next = null;
+    }
+}
---
+++ src/test/java/com/linkedlist/MyNodeTest.java
@@ -0,0 +1,12 @@
+package com.linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+public class MyNodeTest {
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+    }
+}
