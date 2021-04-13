6bb07c16a06aea68636628c384151eb9bc00775f
ganesh-48 <ganeshsrananavare@gmail.com>
2021-04-13 01:37:44 -0700

    ADDED: Create a linked list ina appending order

--- src/main/java/com/linkedlist/MyLinkedList.java
+++ src/main/java/com/linkedlist/MyLinkedList.java
@@ -9,6 +9,9 @@ public class MyLinkedList {
         this.tail = null;
     }
 
+    /*@Description-Created a linked list
+    *Added node in a Linked list
+     */
     public void add(INode myNode) {
         if (this.tail == null) {
             this.tail = myNode;
@@ -22,6 +25,26 @@ public class MyLinkedList {
         }
     }
 
+    /*@Description-Created a linked list in sequence
+    *Added a node in the appeending order
+    * firstly added a 56
+    * then  append 30 to the 56
+    * finally append 30 to 70
+    * Linked list sequence:56->30->70
+     */
+    public void append(INode myNode) {
+        if (this.tail == null) {
+            this.tail = myNode;
+        }
+        if (this.head == null) {
+            this.head = myNode;
+        } else {
+                this.tail.setNext(myNode);
+                this.tail = myNode;
+            }
+    }
+
+    //printed a nodes
     public void printMyNodes() {
         StringBuffer myNodes = new StringBuffer("My Nodes: ");
         INode tempNode = head;
